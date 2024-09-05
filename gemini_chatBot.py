a60ff8bbb786f8975a17722b735e2a9243ba8b55
dmuu44 <144538090+dmuu44@users.noreply.github.com>
2024-09-05 21:45:43 +0530

    Add files via upload

---
+++ gemini_chatBot.py
@@ -0,0 +1,15 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBZV5b-gPzYsD-0K_0nYxwwp4eJGYUhhFk")
+model = genai.GenerativeModel("gemini-1.5-flash")
+print("chatBot is initilized enter exit for stop chating")
+while True:
+    user_input=input("You:")
+    if user_input=="exit":
+        print("chatBot:bye")
+        break
+    else:
+        response=model.generate_content(user_input)
+        print(f"chatBot: {response.text}")
+        
+    
